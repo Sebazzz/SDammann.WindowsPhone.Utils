141dec96d6c12a5a6ee3a2b3e50c021317b2d98a
Sebazzz <triadsebas@gmail.com>
2012-06-28 23:42:31 +0200

    Added common version info

---
+++ src/CommonAssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Resources;
+
+#if DEBUG
+[assembly: AssemblyConfiguration("DEBUG")]
+#else
+[assembly: AssemblyConfiguration("RELEASE")]
+#endif
+[assembly: AssemblyCompany("Sebastiaan Dammann")]
+[assembly: AssemblyProduct("SDammann Windows Phone Utils")]
+[assembly: AssemblyCopyright("Copyright © Sebastiaan Dammann 2012 - 2014")]
+[assembly: AssemblyTrademark("SDammann Windows Phone Utils")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.3.*")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyInformationalVersion("1.3.0.0")]
+[assembly: NeutralResourcesLanguage("en-US")]
--- src/SDammann.Utils/Properties/AssemblyInfo.cs
+++ src/SDammann.Utils/Properties/AssemblyInfo.cs
@@ -1,37 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-using System.Resources;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("SDammann.Utils")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("DevDammann.nl")]
-[assembly: AssemblyProduct("SDammann.Utils")]
-[assembly: AssemblyCopyright("Copyright © DevDammann.nl 2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("71ff0715-0e10-4bf0-ab62-10733b3a49ff")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.3.*")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
-[assembly: NeutralResourcesLanguageAttribute("en-US")]
+[assembly: AssemblyTitle("SDammann Utils")]
+[assembly: AssemblyDescription("Non-backgroundagent safe utilities")]
